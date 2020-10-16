006c02398a6d127a06903542ecbbeb84fcc72156
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-15 22:37:18 -0300

    Controller para os flash cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user_flash_card_path(current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get "flash_cards/index"
   root "home#index"
   devise_for :users, path: "accounts"
+
+  resources :users do
+    resources :flash_cards
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
