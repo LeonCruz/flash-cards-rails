96141e2e4c799c0c5ae1170f96d70dc9cb22d02f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-17 18:09:10 -0300

    Comparação entre a resposta do usuário e a salva no banco

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -10,20 +10,29 @@ class FlashCardsController < ApplicationController
   def create
     @flash_card = current_user.flash_cards.build(flash_card_params)
 
-    respond_to do |format|
-      if @flash_card.save
-        flash[:notice] = "Criado com sucesso"
-        format.html { redirect_to action: "index" }
-      else
-        @flash_card.errors.full_messages.each do |msg|
-        end
-        format.html { render :new }
-      end
+    if @flash_card.save
+      flash[:notice] = "Criado com sucesso"
+      redirect_to action: "index"
+    else
+      render :new
     end
   end
 
   def show
     @flash_card = FlashCard.find(params[:id])
+  end
+
+  def compare
+    your_answer = params[:your_answer]
+    @flash_card = FlashCard.find(params[:flash_card_id])
+
+    flash[:notice] = if your_answer == @flash_card.answer
+      "Você acertou"
+    else
+      "Você errou"
+    end
+
+    redirect_to user_flash_card_path(id: @flash_card.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "flash_cards/index"
   root "home#index"
   devise_for :users, path: "accounts"
 
   resources :users do
-    resources :flash_cards
+    resources :flash_cards do
+      post "compare", to: "flash_cards#compare"
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
