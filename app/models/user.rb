08adf057612f17c5776622c6ced2bde8f627942c
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-25 11:06:32 -0300

    Testes para cadastro do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :flash_cards
+
+  validates_presence_of :email, :encrypted_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'registar um usuário' do
+    user = users :user1
+    assert user.save
+  end
+
+  test 'não registar um usuário sem email' do
+    user = users :user2
+    assert_not user.save
+  end
+
+  test 'não registar um usuário sem senha' do
+    user = users :user3
+    assert_not user.save
+  end
 end
