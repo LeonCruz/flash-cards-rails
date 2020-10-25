e8c32044ce855f4a6a18abac9ed245258c8b62f3
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-25 10:53:31 -0300

    Teste na página inicial

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test 'acessar a págna raiz' do
+    get root_path
     assert_response :success
   end
-
 end
