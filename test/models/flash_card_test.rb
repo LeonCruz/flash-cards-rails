e7434065fe5a1c43e941f0ae1bfb0d68cd49d9b4
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-25 11:14:27 -0300

    Testes para adicinar um flash_card

--- test/models/flash_card_test.rb
+++ test/models/flash_card_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class FlashCardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'adicionar um novo flash card' do
+    flash_card = flash_cards :card1
+    assert flash_card.save
+  end
+
+  test 'não adicionar um flash card sem categoria' do
+    flash_card = flash_cards :card2
+    assert_not flash_card.save
+  end
+
+  test 'não adicionar um flash card sem pergunta' do
+    flash_card = flash_cards :card3
+    assert_not flash_card.save
+  end
+
+  test 'não adicionar um flash card sem resposta' do
+    flash_card = flash_cards :card4
+    assert_not flash_card.save
+  end
 end
